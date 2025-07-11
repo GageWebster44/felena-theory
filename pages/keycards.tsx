50a5e5206244da3a44bca9b074c38fc900c745cf
GageWebster44 <gage9901@gmail.com>
2025-07-11 18:25:03 -0500

    Add files via upload

---
+++ pages/keycards.tsx
@@ -0,0 +1,7 @@
+export default function Keycards() {
+  return (
+    <div className="p-10">
+      <h1 className="text-2xl font-bold">Player Keycard Access</h1>
+    </div>
+  )
+}
---
+++ pages/marketplace.tsx
@@ -0,0 +1,7 @@
+export default function Marketplace() {
+  return (
+    <div className="p-10">
+      <h1 className="text-2xl font-bold">Rent Engines, Purchase XP, Trial</h1>
+    </div>
+  )
+}
